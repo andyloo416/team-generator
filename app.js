966fe803016069693bbef2ca48cedd21d9eca560
andyloo416 <105084947+andyloo416@users.noreply.github.com>
2022-11-24 22:11:49 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,145 +1,209 @@
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
-const Manager = require("./lib/manager");
-const createHTML = require("./createHTML");
-
-const inquirer = require("inquirer");
 const fs = require("fs");
+const inquirer = require("inquirer");
 
-const team = [];
+// Employee template based on these below.
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-const engineerPrompt = () => {
-  return inquirer
+//===================================================================
+// Welcome to a team information HTML generator!
+//===================================================================
+
+// This array fills in with employee data.
+const teamMembers = [];
+// Manager will change-- can't be a const.
+let manager;
+// This info is for the HTML.
+let teamTitle;
+
+//=========================================================
+// First, we prompt the user for the manager/project info.
+//=========================================================
+
+function managerData() {
+  inquirer
     .prompt([
       {
+        // Fill html with teamName.
         type: "input",
-        name: "name",
-        message: "What is the name of the engineer?",
+        message: "What is the name of this team/project?",
+        name: "teamTitle",
+      },
+      {
+        // There is only 1 manager for a team.
+        type: "input",
+        message: "Who is the manager of this project?",
+        name: "managerName",
+      },
+      {
+        // Employee ID.
+        type: "input",
+        message: "What is the manager's ID?",
+        name: "managerID",
+      },
+      {
+        // Employee Email.
+        type: "input",
+        message: "What is the manager's email?",
+        name: "managerEmail",
       },
       {
         type: "input",
-        name: "id",
-        message: "What is the ID of the engineer?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the email of the engineer?",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is the GitHub of the engineer?",
+        message: "What is the manager's office number?",
+        name: "officeNumber",
       },
     ])
-    .then((response) => {
-      const engineer = new Engineer(
-        response.name,
-        response.id,
-        response.email,
-        response.github
+    .then((managerAnswers) => {
+      manager = new Manager(
+        managerAnswers.managerName,
+        managerAnswers.managerID,
+        managerAnswers.managerEmail,
+        managerAnswers.officeNumber
       );
-      team.push(engineer);
-      //   console.log(team);
-      chooseNext();
+      teamTitle = managerAnswers.teamTitle;
+      console.log("Now we will ask for employee information.");
+      lesserEmployeeData();
     });
-};
-
-const chooseNext = () => {
-  return inquirer
+}
+//=================================================================
+// This repeats if more employees need to be added.
+//=================================================================
+function lesserEmployeeData() {
+  inquirer
     .prompt([
       {
         type: "list",
-        name: "role",
-        message: "Choose the next step.",
-        choices: ["new Engineer", "new Manager", "new Intern", "finished"],
+        message: "What is this employee's role?",
+        name: "employeeRole",
+        choices: ["Intern", "Engineer"],
+      },
+
+      //==================================================================
+      // These questions are based on the employeeRole above.
+      //==================================================================
+      {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "employeeName",
+      },
+      {
+        type: "input",
+        message: "What is the employee's id?",
+        name: "employeeId",
+      },
+      {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "employeeEmail",
+      },
+      {
+        type: "input",
+        message: "What is the Engineer's Github?",
+        name: "github",
+        when: (userInput) => userInput.employeeRole === "Engineer",
+      },
+      {
+        type: "input",
+        message: "What's the Intern's school?",
+        name: "school",
+        when: (userInput) => userInput.employeeRole === "Intern",
+      },
+      {
+        type: "confirm",
+        name: "newEmployee",
+        message: "Would you like to add another team member?", // if yes, go back again. If no, renderHTML
       },
     ])
-    .then((response) => {
-      if (response.role == "new Engineer") {
-        engineerPrompt();
-      } else if (response.role == "new Manager") {
-        managerPrompt();
-      } else if (response.role == "new Intern") {
-        internPrompt();
+    .then((answers) => {
+      //============================================================
+      // Pushes a new intern into the team members array
+      //============================================================
+      if (answers.employeeRole === "Intern") {
+        const employee = new Intern(
+          answers.employeeName,
+          answers.employeeId,
+          answers.employeeEmail,
+          answers.school
+        );
+        teamMembers.push(employee);
+      } else if (answers.employeeRole === "Engineer") {
+        // A different way of pushing the info into teamMembers array.
+        teamMembers.push(
+          new Engineer(
+            answers.employeeName,
+            answers.employeeId,
+            answers.employeeEmail,
+            answers.github
+          )
+        );
+      }
+      if (answers.newEmployee === true) {
+        lesserEmployeeData();
       } else {
-        fs.writeFileSync("./createHTML.html");
+        //==================
+        //renderHTML
+        //==================
+
+        var main = fs.readFileSync("./templates/main.html", "utf8");
+        // The slashes and g => regular expressions (regex)
+        // This allows the replace function to replace all occurances of teamTitle.
+        // If I just did '{{teamTitle}}' then it only replaces the first instance.
+        main = main.replace(/{{teamTitle}}/g, teamTitle);
+
+        // Loop through the employees and print out all of their cards without replacing the previous one.
+        var managerCard = fs.readFileSync("./templates/Manager.html", "utf8");
+        managerCard = managerCard.replace("{{name}}", manager.getName());
+        managerCard = managerCard.replace("{{role}}", manager.getRole());
+        managerCard = managerCard.replace("{{id}}", manager.getId());
+        managerCard = managerCard.replace("{{email}}", manager.getEmail());
+        managerCard = managerCard.replace(
+          "{{officeNumber}}",
+          manager.getOfficeNumber()
+        );
+
+        //=====================================================
+        // Append all of the team members after manager
+        //=====================================================
+
+        var cards = managerCard; // Initial cards only has the Manager card info.
+        for (var i = 0; i < teamMembers.length; i++) {
+          var employee = teamMembers[i];
+          // Cards adds and then equals every new employee card info.
+          cards += renderEmployee(employee);
+        }
+
+        // Adds cards to main.html and outputs to team.html.
+        main = main.replace("{{cards}}", cards);
+
+        fs.writeFileSync("./output/team.html", main);
+
+        // Console.log that the html has been generated
+        console.log("The team.html has been generated in output");
       }
     });
-};
+}
 
-const managerPrompt = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the manager?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the ID of the manager?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the email of the manager?",
-      },
-      {
-        type: "input",
-        name: "officeNo",
-        message: "What is the office number of the manager?",
-      },
-    ])
-    .then((response) => {
-      const manager = new Manager(
-        response.name,
-        response.id,
-        response.email,
-        response.officeNo
-      );
-      team.push(manager);
-      console.log(team);
-      chooseNext();
-    });
-};
+// renderEmployee function that is called above.
 
-const internPrompt = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the intern?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the ID of the intern?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the email of the intern?",
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "What is the university of the intern?",
-      },
-    ])
-    .then((response) => {
-      const intern = new Intern(
-        response.name,
-        response.id,
-        response.email,
-        response.school
-      );
-      team.push(intern);
-      console.log(team);
-      chooseNext();
-    });
-};
+function renderEmployee(employee) {
+  if (employee.getRole() === "Intern") {
+    var internCard = fs.readFileSync("./templates/Intern.html", "utf8");
+    internCard = internCard.replace("{{name}}", employee.getName());
+    internCard = internCard.replace("{{role}}", employee.getRole());
+    internCard = internCard.replace("{{id}}", employee.getId());
+    internCard = internCard.replace("{{email}}", employee.getEmail());
+    internCard = internCard.replace("{{school}}", employee.getSchool());
+    return internCard;
+  } else if (employee.getRole() === "Engineer") {
+    var engineerCard = fs.readFileSync("./templates/Engineer.html", "utf8");
+    engineerCard = engineerCard.replace("{{name}}", employee.getName());
+    engineerCard = engineerCard.replace("{{role}}", employee.getRole());
+    engineerCard = engineerCard.replace("{{id}}", employee.getId());
+    engineerCard = engineerCard.replace("{{email}}", employee.getEmail());
+    engineerCard = engineerCard.replace("{{github}}", employee.getGithub());
+    return engineerCard;
+  }
+}
 
-engineerPrompt();
+managerData();
